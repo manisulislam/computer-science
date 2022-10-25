b537ee53318cf35dfa3065fd96ec5bda0db278b2
anisul islam <anisulislam980340@gmail.com>
2022-10-25 10:07:38 +0600

    log in and register setup with solved problem

--- src/components/Register.js
+++ src/components/Register.js
@@ -35,7 +35,7 @@ const Register = () => {
           </label>
         </div>
         <div className="form-control mt-6">
-          <button className="btn btn-primary">Login</button>
+          <button className="btn btn-primary">Register</button>
         </div>
       </div>
     </div>
